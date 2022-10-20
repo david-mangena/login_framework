70af2a77c0d67e04b3db6bb5cb83b32fe14b5b42
David M <dvdmangena@gmail.com>
2022-10-20 17:36:34 +0200

    removed blank spaces and added headless

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -7,12 +7,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.io.FileHandler;
 import org.testng.ITestResult;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
@@ -26,7 +23,7 @@ public class BaseTests {
     @BeforeMethod
     public void setup() {
         ChromeOptions options = new ChromeOptions();
-//        options.addArguments("--headless");
+        options.addArguments("--headless");
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
--- src/test/java/base/ForgotPwdTest.java
+++ src/test/java/base/ForgotPwdTest.java
@@ -5,12 +5,9 @@ import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pages.ForgotPasswordPage;
 
-
 public class ForgotPwdTest extends BaseTests {
-
     @Test(priority = 2, dataProvider = "LoginData")
     public void forgotPwdTest(String LinkText, String Email, boolean ErrorMessage){
-
         ForgotPasswordPage forgotPasswordPage = homePage.clickForgotPwd(LinkText);
         forgotPasswordPage.enterEmail(Email);
         forgotPasswordPage.clickRetrvPwdBtn();
@@ -27,5 +24,4 @@ public class ForgotPwdTest extends BaseTests {
         data[3][0] = "Forgot Password"; data[3][1] = "just@do.it"; data[3][2] = false;
         return data;
     }
-
 }
